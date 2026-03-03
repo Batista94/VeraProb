a3c64513b707d8085956acc8eb8044cdc05c7fb8
Weslei Batista <wesbatistaa@gmail.com>
2026-03-03 17:44:04 -0300

    audit: remove orphaned B2C driver_app_mvp.dart and empty stops/presentation dir

--- lib/core/geolocation/geo_locator_stub.dart
+++
@@ -1,5 +0,0 @@
-import 'geo_locator.dart';
-
-// This function throws an error if neither mobile nor web implementation is loaded
-GeoLocatorService getGeoLocator() =>
-    throw UnsupportedError('Cannot create a GeoLocatorService');
