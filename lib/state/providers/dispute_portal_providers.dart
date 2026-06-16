f5b5032e14a715f0e29b8719f6f2a2ae70653a88
Weslei Batista <wesbatistaa@gmail.com>
2026-06-16 14:01:42 -0300

    feat: implement platform-aware file hashing and Riverpod providers for the dispute portal

--- lib/state/providers/dispute_portal_providers.dart
+++ lib/state/providers/dispute_portal_providers.dart
@@ -7,7 +7,8 @@ import 'package:veraprob/application/dispute_portal/portal_submission_audit_gate
 import 'package:veraprob/application/dispute_portal/infraction_context_projection.dart';
 import 'package:veraprob/application/dispute_portal/portal_snapshot.dart';
 import 'package:veraprob/infrastructure/dispute_portal/chunked_file_hasher.dart';
-import 'package:veraprob/infrastructure/dispute_portal/web_worker_file_hasher.dart';
+import 'package:veraprob/infrastructure/dispute_portal/web_worker_file_hasher_stub.dart'
+    if (dart.library.js_interop) 'package:veraprob/infrastructure/dispute_portal/web_worker_file_hasher.dart';
 import 'package:veraprob/infrastructure/dispute_portal/supabase_portal_dispute_gateway.dart';
 import 'package:veraprob/infrastructure/dispute_portal/supabase_portal_submission_audit_gateway.dart';
 import 'package:veraprob/infrastructure/providers/supabase_provider.dart';
